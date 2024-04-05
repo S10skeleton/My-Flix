eeebe9dce4efc13d6fd982e11cbc34323ecef138
Shane <s10skeleton@gmail.com>
2024-04-05 02:24:48 -0600

    more render changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myflix');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://s10skeleton:Goesboom1!@cluster0.kbvfy0p.mongodb.net/');
 
 module.exports = mongoose.connection;
