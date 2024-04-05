d6aaeda40d13c3c6f3b9433cf5a5cd928dc7321d
Shane <s10skeleton@gmail.com>
2024-04-05 14:04:10 -0600

    added security features

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Login from '../components/Login';
-import Signup from '../components/Signup';
+// import Signup from '../components/Signup';
 
 const AuthPage = () => {
   return (
     <div className="auth-page">
       <Login />
-      <Signup />
     </div>
   );
 };
