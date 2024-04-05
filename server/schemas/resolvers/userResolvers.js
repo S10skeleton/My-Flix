c2a0e8440c1a60adbe943c5c30c4e495e102972f
Shane <s10skeleton@gmail.com>
2024-04-05 02:48:31 -0600

    more package changes for render

--- server/schemas/resolvers/userResolvers.js
+++ server/schemas/resolvers/userResolvers.js
@@ -1,5 +1,5 @@
 const { User, Movie } = require("../../models");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { signToken } = require("../../utils/Auth");
 
