d9cfffb62d4c2cc4cbe0b014229d9653ab86f984
Shane <s10skeleton@gmail.com>
2024-06-23 23:00:55 -0600

    URI update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'your-default-mongodb-uri', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://s10skeleton:Goesboom1!@cluster0.kbvfy0p.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
