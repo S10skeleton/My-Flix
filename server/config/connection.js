8cb06b0abaff08e3b5ee5aa31468038a2d45d286
Shane <s10skeleton@gmail.com>
2024-06-23 23:58:13 -0600

    connection update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://s10skeleton:Goesboom1!@cluster0.kbvfy0p.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://s10skeleton:Goesboom1!@cluster0.kbvfy0p.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Mongoose connection error:', err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+connectDB();
 
 module.exports = mongoose.connection;
